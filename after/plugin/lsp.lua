81919133fdcb20e933d8d82d7dad1fa8f7180569
Bogdan Culda <culda@proton.me>
2024-11-07 10:08:04 +0000

    stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,66 +5,66 @@ local inoremap = Remap.inoremap
 -- Setup nvim-cmp.
 local cmp = require("cmp")
 local source_mapping = {
-    youtube = "[Suck it YT]",
-    buffer = "[Buffer]",
-    nvim_lsp = "[LSP]",
-    nvim_lua = "[Lua]",
-    -- cmp_tabnine = "[TN]",
-    path = "[Path]",
+  youtube = "[Suck it YT]",
+  buffer = "[Buffer]",
+  nvim_lsp = "[LSP]",
+  nvim_lua = "[Lua]",
+  -- cmp_tabnine = "[TN]",
+  path = "[Path]",
 }
 local lspkind = require("lspkind")
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            -- For `vsnip` user.
-            -- vim.fn["vsnip#anonymous"](args.body)
+  snippet = {
+    expand = function(args)
+      -- For `vsnip` user.
+      -- vim.fn["vsnip#anonymous"](args.body)
 
-            -- For `luasnip` user.
-            require("luasnip").lsp_expand(args.body)
+      -- For `luasnip` user.
+      require("luasnip").lsp_expand(args.body)
 
-            -- For `ultisnips` user.
-            -- vim.fn["UltiSnips#Anon"](args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-    }),
+      -- For `ultisnips` user.
+      -- vim.fn["UltiSnips#Anon"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+  }),
 
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.kind = lspkind.presets.default[vim_item.kind]
-            local menu = source_mapping[entry.source.name]
-            -- if entry.source.name == "cmp_tabnine" then
-            --   if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-            --     menu = entry.completion_item.data.detail .. " " .. menu
-            --   end
-            --   vim_item.kind = "@"
-            -- end
-            vim_item.menu = menu
-            return vim_item
-        end,
-    },
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = lspkind.presets.default[vim_item.kind]
+      local menu = source_mapping[entry.source.name]
+      -- if entry.source.name == "cmp_tabnine" then
+      --   if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+      --     menu = entry.completion_item.data.detail .. " " .. menu
+      --   end
+      --   vim_item.kind = "@"
+      -- end
+      vim_item.menu = menu
+      return vim_item
+    end,
+  },
 
-    sources = {
-        { name = "nvim_lsp" },
+  sources = {
+    { name = "nvim_lsp" },
 
-        -- For vsnip user.
-        -- { name = 'vsnip' },
+    -- For vsnip user.
+    -- { name = 'vsnip' },
 
-        -- For luasnip user.
-        { name = "luasnip" },
+    -- For luasnip user.
+    { name = "luasnip" },
 
-        -- For ultisnips user.
-        -- { name = 'ultisnips' },
+    -- For ultisnips user.
+    -- { name = 'ultisnips' },
 
-        { name = "buffer" },
+    { name = "buffer" },
 
-        { name = "youtube" },
-    },
+    { name = "youtube" },
+  },
 })
 
 -- local tabnine = require("cmp_tabnine.config")
@@ -77,39 +77,40 @@ cmp.setup({
 -- })
 
 local function config(_config)
-    return vim.tbl_deep_extend("force", {
-        on_attach = function(client, bufnr)
-            -- Add Go-specific keybinding only for Go files
-            if client.name == "gopls" then
-                vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>got', ':GoTestFile -v <CR>',
-                    { noremap = true, silent = true })
+  return vim.tbl_deep_extend("force", {
+    on_attach = function(client, bufnr)
+      -- Add Go-specific keybinding only for Go files
+      if client.name == "gopls" then
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>got', ':GoTestFile -v <CR>',
+          { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>got', ':vert copen 40<CR>', { noremap = true, silent = true })
+      end
+
+      nnoremap("gd", function() vim.lsp.buf.definition() end)
+      nnoremap("gD", function() vim.lsp.buf.declaration() end)
+      nnoremap("K", function() vim.lsp.buf.hover() end)
+      nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+      nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
+      nnoremap("[d", function() vim.diagnostic.goto_next() end)
+      nnoremap("]d", function() vim.diagnostic.goto_prev() end)
+      nnoremap("<leader>ca", function() vim.lsp.buf.code_action() end)
+      nnoremap("<leader>co", function() vim.lsp.buf.code_action({
+          filter = function(code_action)
+            if not code_action or not code_action.data then
+              return false
             end
 
-            nnoremap("gd", function() vim.lsp.buf.definition() end)
-            nnoremap("gD", function() vim.lsp.buf.declaration() end)
-            nnoremap("K", function() vim.lsp.buf.hover() end)
-            nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-            nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
-            nnoremap("[d", function() vim.diagnostic.goto_next() end)
-            nnoremap("]d", function() vim.diagnostic.goto_prev() end)
-            nnoremap("<leader>ca", function() vim.lsp.buf.code_action() end)
-            nnoremap("<leader>co", function() vim.lsp.buf.code_action({
-                    filter = function(code_action)
-                        if not code_action or not code_action.data then
-                            return false
-                        end
-
-                        local data = code_action.data.id
-                        return string.sub(data, #data - 1, #data) == ":0"
-                    end,
-                    apply = true
-                })
-            end)
-            nnoremap("<leader>i", function() vim.lsp.buf.references() end)
-            nnoremap("<leader>rn", function() vim.lsp.buf.rename() end)
-            inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
-        end,
-    }, _config or {})
+            local data = code_action.data.id
+            return string.sub(data, #data - 1, #data) == ":0"
+          end,
+          apply = true
+        })
+      end)
+      nnoremap("<leader>i", function() vim.lsp.buf.references() end)
+      nnoremap("<leader>rn", function() vim.lsp.buf.rename() end)
+      inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
+    end,
+  }, _config or {})
 end
 
 require("mason").setup {}
@@ -123,12 +124,12 @@ require("lspconfig").zls.setup(config())
 require("lspconfig").ts_ls.setup(config())
 
 require("lspconfig").eslint.setup(config({
-    settings = {
-        codeActionOnSave = {
-            enable = true,
-            mode = "all"
-        },
-    }
+  settings = {
+    codeActionOnSave = {
+      enable = true,
+      mode = "all"
+    },
+  }
 }))
 
 require("lspconfig").jsonls.setup(config())
@@ -144,24 +145,24 @@ require("lspconfig").solang.setup(config())
 require("lspconfig").cssls.setup(config())
 
 require("lspconfig").gopls.setup(config({
-    cmd = { "gopls", "serve" },
-    settings = {
-        gopls = {
-            analyses = {
-                unusedparams = true,
-            },
-            staticcheck = true,
-            gofumpt = true,
-            completeUnimported = true,
-            usePlaceholders = true,
-        },
+  cmd = { "gopls", "serve" },
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+      completeUnimported = true,
+      usePlaceholders = true,
     },
+  },
 }))
 
 -- who even uses this?
 require("lspconfig").rust_analyzer.setup(config({
-    cmd = { "rustup", "run", "nightly", "rust-analyzer" },
-    --[[
+  cmd = { "rustup", "run", "nightly", "rust-analyzer" },
+  --[[
     settings = {
         rust = {
             unstable_features = true,
@@ -173,84 +174,84 @@ require("lspconfig").rust_analyzer.setup(config({
 }))
 
 require("lspconfig").lua_ls.setup(config({
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
     },
+  },
 }))
 
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    underline = true,
-    update_in_insert = true,
-    severity_sort = false,
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = false,
 })
 
 local opts = {
-    -- whether to highlight the currently hovered symbol
-    -- disable if your cpu usage is higher than you want it
-    -- or you just hate the highlight
-    -- default: true
-    highlight_hovered_item = true,
+  -- whether to highlight the currently hovered symbol
+  -- disable if your cpu usage is higher than you want it
+  -- or you just hate the highlight
+  -- default: true
+  highlight_hovered_item = true,
 
-    -- whether to show outline guides
-    -- default: true
-    show_guides = true,
+  -- whether to show outline guides
+  -- default: true
+  show_guides = true,
 }
 
 require("symbols-outline").setup(opts)
 
 local snippets_paths = function()
-    local plugins = { "friendly-snippets" }
-    local paths = {}
-    local path
-    local root_path = vim.env.HOME .. "/.vim/plugged/"
-    for _, plug in ipairs(plugins) do
-        path = root_path .. plug
-        if vim.fn.isdirectory(path) ~= 0 then
-            table.insert(paths, path)
-        end
+  local plugins = { "friendly-snippets" }
+  local paths = {}
+  local path
+  local root_path = vim.env.HOME .. "/.vim/plugged/"
+  for _, plug in ipairs(plugins) do
+    path = root_path .. plug
+    if vim.fn.isdirectory(path) ~= 0 then
+      table.insert(paths, path)
     end
-    return paths
+  end
+  return paths
 end
 
 require("luasnip.loaders.from_vscode").lazy_load({
-    paths = snippets_paths(),
-    include = nil, -- Load all languages
-    exclude = {},
+  paths = snippets_paths(),
+  include = nil, -- Load all languages
+  exclude = {},
 })
 
 require("mason-null-ls").setup({
-    ensure_installed = { "black", "eslint" }
+  ensure_installed = { "black", "eslint" }
 })
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.formatting.black,
-        -- null_ls.builtins.code_actions.eslint_d,
-        -- null_ls.builtins.code_actions.eslint
-    },
+  sources = {
+    null_ls.builtins.diagnostics.golangci_lint,
+    null_ls.builtins.formatting.black,
+    -- null_ls.builtins.code_actions.eslint_d,
+    -- null_ls.builtins.code_actions.eslint
+  },
 })
 
 -- cleras the lightbulb
